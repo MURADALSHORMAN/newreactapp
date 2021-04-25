66c6a4a6c65659775b516e400a416e262fc818aa
murad alshorman <muradalshorman@gmail.com>
2021-04-25 23:40:32 +0300

    new

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+
+
+import React from  'react';
+import Button from './component/button';
+
+class App extends React.Component{
+render() {
+return(
+  <div>
+  <h1> hello  </h1>
+  <p> how are you?</p>
+  <Button/>
+  
+
+
+
+  </div>
+)
+
+}
+}
+
+export default App;
+
